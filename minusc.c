16fc39d1b5d14fabfa0a9519c6014f5452f3d80c
Keithuu <tonny_ctl@hotmail.com>
2017-12-07 15:04:39 +0000

    Add minusc.c

---
+++ minusc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define MAXSTR 500
+int main() {
+    char str[MAXSTR];
+    char diff = 'a' - 'A';
+    while (fgets(str, MAXSTR, stdin) != NULL) {
+        for (int i = 0; str[i] != '\0'; ++i) {
+            if ((str[i] >= 'A') && (str[i] <= 'Z'))
+                printf("%c", str[i] + diff);
+            else
+                printf("%c", str[i]);
+        }
+    }
+
+}
